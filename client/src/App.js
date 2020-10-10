4d5ccd584f775bb2603684a376904ed3447b4c7c
Pochobs <pochobs@hotmail.com>
2020-10-09 21:55:55 -0500

    Module 21.4.3 routes are running well

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // add these two library import statements
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 
 // we establish a new connection to the GraphQL server using Apollo.
 const client = new ApolloClient({
@@ -18,13 +23,21 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/thought" component={SingleThought} />
+
+            <Route component={NoMatch} />
+          </Switch>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
